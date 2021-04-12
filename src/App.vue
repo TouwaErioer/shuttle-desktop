c83216fe8eb6b95812770e4a34c54fdb74182e35
TouwaErioer <duhongyu627@gmail.com>
2021-04-12 10:15:51 +0800

    feat:添加搜索页

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-    <router-view/>
+    <transition name="custom-classes-transition" :enter-active-class="enterTransitionName">
+        <router-view/>
+    </transition>
 </template>
 
 <script>
-
     export default {
         name: 'App',
+        data() {
+            return {
+                enterTransitionName: null,
+            }
+        },
+        watch: {
+            '$route'(to) {
+                this.enterTransitionName = to.path === '/search' ? 'animated slideInDown' : null;
+            }
+        }
     }
 </script>
 
 <style>
     @import "../src/style/cover.css";
+
+    @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 </style>
